a7dc1032f5d5b53f02c4d9252427e5879fe1ca0e
GRUPPOGPI\u24067 <lorenzo.parolini@itstechtalentfactory.it>
2025-08-21 09:45:32 +0200

    feat: aggiunta navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Regioni } from './components/regioni/regioni';
+import { Main } from './components/main/main';
+import { Clienti } from './components/clienti/clienti';
+import { Ambienti } from './components/ambienti/ambienti';
+import { Software } from './components/software/software';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '',  component: Main },
+
+    { path: 'home', component: Main },
+    { path: 'regione', component: Regioni },
+    { path: 'cliente', component: Clienti },
+    { path: 'ambiente', component: Ambienti },
+    { path: 'software', component: Software },
+    
+    { path: '**', redirectTo: '' }
+];
--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, FormsModule],
   templateUrl: './navbar.html',
   styleUrls: ['./navbar.css']
 })
 export class Navbar {
-
+  searchQuery: string = '';
+  toggleMenu() {
+    // Logica per aprire/chiudere il menu
+    console.log('Menu toggled');
+  }
 }
