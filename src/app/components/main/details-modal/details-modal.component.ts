4edaf8d5b17b2a667e200ddefd513fc75177d4a0
GRUPPOGPI\u24067 <lorenzo.parolini@itstechtalentfactory.it>
2025-09-05 16:53:39 +0200

    feat: aggiunta modale dettagli

---
+++ src/app/components/main/details-modal/details-modal.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Cliente } from '../../../models/cliente.model';
+import { Software } from '../../../models/software.model';
+import { Ambiente } from '../../../models/ambiente.model';
+
+@Component({
+  selector: 'app-details-modal',
+  imports: [CommonModule],
+  templateUrl: './details-modal.component.html',
+  styleUrl: './details-modal.component.css'
+})
+export class DetailsModalComponent {
+  @Input() cliente: Cliente | null = null;
+  @Input() isOpen: boolean = false;
+  @Output() closeModal = new EventEmitter<void>();
+
+  onBackdropClick(event: MouseEvent) {
+    if (event.target === event.currentTarget) {
+      this.closeModal.emit();
+    }
+  }
+
+  onCloseClick() {
+    this.closeModal.emit();
+  }
+
+  trackBySoftwareId(index: number, software: Software): string {
+    return software.id;
+  }
+
+  trackByAmbienteId(index: number, ambiente: Ambiente): string {
+    return ambiente.id;
+  }
+}
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -4,10 +4,11 @@ import { ClientiService } from '../../services/clienti.service';
 import { Cliente } from '../../models/cliente.model';
 
 import { LoadingSpinnerComponent } from '../loading-spinner/loading-spinner.component';
+import { DetailsModalComponent } from './details-modal/details-modal.component';
 
 @Component({
   selector: 'app-main',
-  imports: [CommonModule, LoadingSpinnerComponent],
+  imports: [CommonModule, LoadingSpinnerComponent, DetailsModalComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
@@ -15,6 +16,8 @@ export class MainComponent implements OnInit {
   clienti: Cliente[] = [];
   selectedCliente: Cliente | null = null;
   loading: boolean = true;
+  isModalOpen: boolean = false;
+  modalCliente: Cliente | null = null;
 
   constructor(private clientiService: ClientiService) {}
 
@@ -36,5 +39,15 @@ export class MainComponent implements OnInit {
 
   showDetails(cliente: Cliente) {
     this.selectedCliente = cliente;
+  }
+
+  openModal(cliente: Cliente) {
+    this.modalCliente = cliente;
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+    this.modalCliente = null;
   }
 }
